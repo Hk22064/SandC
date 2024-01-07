dfe0d95eb438cb1cd286ba89576d3f2161c89e2c
Hk22064 <146146098+Hk22064@users.noreply.github.com>
2024-01-07 23:48:20 +0900

    Update savePassword.mjs

--- netlify/functions/savePassword.mjs
+++ netlify/functions/savePassword.mjs
@@ -3,14 +3,13 @@
 exports.handler = async (event) => {
   try {
     const { password } = JSON.parse(event.body);
-    const data = {
-      message: "Hello, World!",
-    };
+    const savedPassword = password; 
     // ここでパスワードを保存するデータベースやストレージなどに送信する処理を実行する
 
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Password saved successfully' }),
+      body: JSON.stringify({ savedPassword }),
     };
   } catch (error) {
     return {

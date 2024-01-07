f31bffdafecdd66d7839512bf9784a2179146fb2
Hk22064 <146146098+Hk22064@users.noreply.github.com>
2024-01-07 14:21:26 +0900

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // App.jsx
 
 import React from 'react';
+import PasswordComponent from './components/PasswordComponent'; 
 import PasswordForm from './components/PasswordForm'; // 作成したパスワードフォームコンポーネント
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <div>
       <h1>Password Manager App</h1>
       <PasswordForm /> {/* 作成したパスワードフォームを呼び出す */}
+      <PasswordComponent />
     </div>
   );
 };
